1d8e5b47be3e2c36489829478d63d442d4fe302c
Asad Nawaz <139525501+Asad-Nawaz92@users.noreply.github.com>
2023-08-12 21:35:47 +0500

    Using Nested For Loop

---
+++ app.js
@@ -0,0 +1,54 @@
+// -------> Imp Aesteric Patterns <---------
+
+//    *
+//    **
+//    ***
+//    ****
+//    *****
+
+// ------> Code <--------
+
+for(var i=1; i<=5; i++){
+    for(var j=1; j<=i; j++)
+    {
+        document.write("*")
+    }
+    document.write("<br>")
+}
+
+document.write("<br>")
+
+// *****
+// ****
+// ***
+// **
+// *
+
+// ------> Code <------
+
+for (var i = 1; i <= 5; i++) {
+    for (var j = i; j <= 5; j++) {
+        document.write("*")
+    }
+    document.write("<br>")
+}
+
+document.write("<br>")
+
+for (var i = 1; i <= 5; i++) {
+    for (var j = 1; j <= 5; j++) {
+        document.write(i)
+    }
+    document.write("<br>")
+}
+
+document.write("<br>")
+
+for (var i = 5; i >= 1; i--) {
+    for (var j = 1; j <= 5; j++) {
+        document.write(i)
+    }
+    document.write("<br>")
+}
+
+document.write("<br>")
